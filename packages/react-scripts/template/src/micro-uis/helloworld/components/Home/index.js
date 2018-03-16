07183cebf6868573b2dee576c37f7e9cd2b431ef
Lee Francis <lfrancis@ebsco.com>
2018-03-15 23:02:36 -0400

    bump version and modify text

--- packages/react-scripts/template/src/micro-uis/helloworld/components/Home/index.js
+++ packages/react-scripts/template/src/micro-uis/helloworld/components/Home/index.js
@@ -9,11 +9,10 @@ const Home = () => (
     <h1 level="1">Hello World Home Page</h1>
     <p>This is the Hello World home page</p>
     <p>
-      See
-      <a href="https://github.com/EBSCOIS/experiment.shared.ui/tree/MicroUI-Example">
-        Micro-UI Branch
-      </a>
-      branch for a slightly more involved example of Micro UI code organization.
+      It shows how each subsection of the app (MicroUI) can have its own route,
+      components, containers and reducer handling A CodeOwners file can be
+      created at the route that will specify the owners for this section of the
+      app.
     </p>
     <p>
       And Example of a shared Tool
